c39ac442027acfd790fb19873070dcb11f2952d1
thanmayi22 <darshathanmayi@gmail.com>
2026-01-07 21:32:44 +0530

    Milestone 1

---
+++ main.py
@@ -0,0 +1,18 @@
+from semantic_search import fetch_papers
+from dataset import prepare_dataset, save_dataset
+import json
+def show_papers(papers):
+    for i, paper in enumerate(papers, start=1):
+        print(f"\nPaper {i}")
+        print("Title:", paper.get("title"))
+        print("Year:", paper.get("year"))
+        print("Link:", paper.get("url"))
+        print("-" * 40)
+
+topic = input("Enter research topic: ")
+limit = int(input("Enter number of papers: "))
+
+papers = fetch_papers(topic, limit)
+show_papers(papers)
+dataset = prepare_dataset(papers)
+save_dataset(dataset)
---
+++ semantic_search.py
@@ -0,0 +1,25 @@
+import time
+import requests                         # pyright: ignore[reportMissingModuleSource]
+import json
+
+def fetch_papers(topic, limit=5):
+    url = "https://api.semanticscholar.org/graph/v1/paper/search"
+    
+    params = {
+        "query": topic,
+        "limit": limit,
+        "fields": "title,authors,year,abstract,url"
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        return response.json()["data"]
+
+    elif response.status_code == 429:
+        print("Rate limit hit. Waiting...")
+        time.sleep(5)
+        return fetch_papers(topic, limit)
+
+    else:
+        response.raise_for_status()
